e61c73ef967d830bed2e12b37ccfd3a7230cfd57
dhav211 <dhav211@gmail.com>
2024-02-27 16:42:58 -0800

    Add ability to post messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
   def index
-    # TODO: show the posts
+    @posts = Post.all
+    @new_post = Post.new
+  end
+
+  def create
+    post = Post.new text: params[:post][:text], user: current_user
+
+    if post.save
+      flash[:notice] = 'Post was a success!'
+    else
+      flash[:alert] = 'Post was a failure!'
+    end
+
+    redirect_to root_path
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+  def hide_users_name(users_name)
+    hidden_name = ''
+    users_name.split('').each { |_| hidden_name += '?' }
+    hidden_name
+  end
 end
